0327647ca5719352806eab742c2b7160f6d65661
MZC01-SYKANG <sykang@mz.co.kr>
2022-09-20 16:58:17 +0900

    test

--- src/main/java/com/sykang/swaggertest/service/awsServiceImpl.java
+++ src/main/java/com/sykang/swaggertest/service/awsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.sykang.swaggertest.service;
 
 import com.sykang.swaggertest.dao.mariadbDao;
-import com.sykang.swaggertest.vo.describeAlarmsVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
